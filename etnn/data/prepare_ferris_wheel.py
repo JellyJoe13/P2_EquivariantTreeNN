61609d8be096f9fd854b8354f8a1d782a9d3d150
JellyJoe13 <johannes.p.urban@gmail.com>
2023-10-27 18:46:52 +0200

    created dataset creation

--- etnn/data/prepare_ferris_wheel.py
+++ etnn/data/prepare_ferris_wheel.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import os
 from etnn.data import DEFAULT_DATA_PATH
+from tqdm import tqdm
+from etnn.data.ferris_score_helpers import build_wheel_happyness
 
 
 def prepare_1_ferris(
@@ -69,4 +71,59 @@ def prepare_1_ferris(
     return df
 
 
+def generate_ferris_dataset(
+        num_gondolas: int = 10,
+        num_part_pg: int = 5,
+        num_to_generate: int = 1_000,
+        df_name_input: str = "Sleep_health_and_lifestyle_dataset.csv",
+        dataset_path: str = DEFAULT_DATA_PATH,
+        df_intermediate_output_name: str = 'health_dataset_preprocessed-1.csv',
+        try_pregen: bool = True
+):
+    # see if file already exists and load this
+    # file name logic
+    file_name = f"ferris-wheel_g-{num_gondolas}_p-{num_part_pg}_size-{num_to_generate}"
+    file_path = os.path.join(dataset_path, file_name)
 
+    if os.path.isfile(file_path):
+        return pd.read_csv(file_path, index_col=0)
+
+    # else generate it
+    # load dataset which is the base of the data generation
+    df = prepare_1_ferris(
+        df_name_input=df_name_input,
+        dataset_path=dataset_path,
+        df_name_output=df_intermediate_output_name,
+        try_pregen=try_pregen
+    )
+
+    # init ordering array
+    random_order = np.arange(len(df))
+    # initialize dataset storage
+    dataset_storage = []
+
+    # produce a number of elements
+    for _ in range(tqdm(range(num_to_generate))):
+        # generate sample element
+        sample = random_order[:num_gondolas*num_part_pg].reshape(num_gondolas, num_part_pg)
+
+        # calc label
+        label = build_wheel_happyness(df, sample)
+
+        # add to storage
+        dataset_storage.append(sample.flatten().tolist() + [label])
+
+    # fuze dataset
+    df_generated = pd.DataFrame(
+        dataset_storage,
+        columns=[
+            f"g-{i}_p-{j}"
+            for i in range(num_gondolas)
+            for j in range(num_part_pg)
+        ] + ['label']
+    )
+
+    # save dataset
+    df_generated.to_csv(file_path)
+
+    return df_generated
