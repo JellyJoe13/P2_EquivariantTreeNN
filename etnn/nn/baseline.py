96f4745783c00198fbfba373afb70da3f2034388
JellyJoe13 <johannes.p.urban@gmail.com>
2023-11-09 20:50:48 +0100

    Wrote documentation

--- etnn/nn/baseline.py
+++ etnn/nn/baseline.py
@@ -12,6 +12,32 @@ def create_baseline_model(
         iteration_increase: float = 0.1,
         start_factor: float = 2.
 ) -> typing.Tuple[torch.nn.Sequential, bool]:
+    """
+    Creates a baseline model consisting of linear layers and relu activation functions in between.
+
+    :param n_params: number of parameter the original model has
+    :type n_params: int
+    :param input_dim: input dimension this model should have. should be number of elements in the input sequence
+        times the dimension of each element
+    :type input_dim: int
+    :param output_dim: dimension of the output
+    :type output_dim: int
+    :param n_layer: number of layers the model should have
+    :type n_layer: int
+    :param tolerance: factor by how much the number of parameters may be exceeded (only roughly as the RELU layer
+        parameters are not accounted for here)
+    :type tolerance: float
+    :param end_tolerance: at the end the model parameters of the constructed model will be checked again. This
+        parameter controls the parameter by which factor the parameter count may exceed the input parameters with
+        still being considered a model one could and should use
+    :type end_tolerance: float
+    :param iteration_increase: value by how much the factor increases in each iteration
+    :type iteration_increase: float
+    :param start_factor: value controlling with which value the factor should start
+    :type start_factor: float
+    :return: model and bool whether the model should be used or has too many parameters
+    :rtype: typing.Tuple[torch.nn.Sequential, bool]
+    """
     # for this we want to use a factor that is closes to producing the wanted parameter count
 
     # init factor
@@ -79,4 +105,12 @@ def create_baseline_model(
 def calc_params(
         model: torch.nn.Module
 ) -> int:
+    """
+    Small function calculating the total number of parameters of a model.
+
+    :param model: Model for which to count the parameters for
+    :type: torch.nn.Module
+    :return: Number of parameters
+    :rtype: int
+    """
     return sum([p.numel() for p in model.parameters()])
