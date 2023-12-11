335d4047706c26831369b7e5bfd2c07c6f941dba
JellyJoe13 <johannes.p.urban@gmail.com>
2023-12-11 19:01:45 +0100

    updated installation script

--- env_setup/conda_cuda12-1_setup.bat
+++ env_setup/conda_cuda12-1_setup.bat
@@ -14,19 +14,22 @@ if /i !"%answer%"=="y" (
 
 echo Proceeding installation...
 echo + Creation of environment...
-call conda create -n "P2" python==3.9.* --yes
+call conda create -n "P2" python==3.9.18 --yes
 
 echo + Activating environment
 call conda activate P2
 
 echo + Installing basic libraries...
-call conda install -y numpy pandas
+call conda install -y numpy=3.9.18 pandas=2.0.3
 
 echo + Installing pytorch...
 call conda install -y pytorch=2.1.0 torchvision=0.16.0 torchaudio=2.1.0 pytorch-cuda=12.1 -c pytorch -c nvidia
 
 echo + Installing advanced libraries...
 call conda install -y matplotlib=3.7.2 scikit-learn=1.3.0 jupyterlab=3.6.3 multiprocess=0.70.15
+
+echo + Installing hyperparameter framework and parallelization
+call conda install -y optuna=3.4.0 plotly=5.9.0 multiprocess=0.70.15 tqdm=4.65.0
 
 echo + Installing libraries for documentation generation
 call conda install -y sphinx=7.2.6 sphinx-rtd-theme=1.3.0
